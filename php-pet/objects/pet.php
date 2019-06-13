311f7256caae821e6e88b3c14b06cd38c86e99ae
mudman28 <johnnie.walters@gmail.com>
2019-06-13 14:36:13 -0500

    working on post

--- php-pet/objects/pet.php
+++ php-pet/objects/pet.php
@@ -37,7 +37,26 @@ class Pet {
     }
 
     function create() {
-
+           // query to insert record
+        $query = INSERT INTO pet (name, breed, color, is_checked_in, owner_id)
+        VALUES (:pet_name, :pet_color, :pet_breed, :checked_in);";
+        // prepare query
+        $stmt = $this->conn->prepare($query);
+        // sanitize
+        $this->pet_name = htmlspecialchars(strip_tags($this->pet_name));
+        $this->pet_color = htmlspecialchars(strip_tags($this->pet_color));
+        $this->pet_breed = htmlspecialchars(strip_tags($this->pet_breed));
+        $this->checked_in = htmlspecialchars(strip_tags($this->checked_in));
+        // bind values
+        $stmt->bindParam(':pet_name', $this->pet_name);
+        $stmt->bindParam(':pet_color', $this->pet_color);
+        $stmt->bindParam(':pet_breed', $this->pet_breed);
+        $stmt->bindParam(':checked_in', $this->checked_in);
+        // execute query
+        if ($stmt->execute()) {
+            return true;
+        }
+        return false;
     }
 
     function delete() {
